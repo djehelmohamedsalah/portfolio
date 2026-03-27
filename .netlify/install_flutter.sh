98658c1afe8aff7cc0e3963236cc78f38599a9f7
mohamedsalah <djehelmohamedsalah@gmail.com>
2026-03-27 15:58:43 +0100

    ci: Add Netlify build script to install Flutter and build the web project.

--- .netlify/install_flutter.sh
+++ .netlify/install_flutter.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing Flutter..."
+FLUTTER_VERSION=${FLUTTER_VERSION:-3.24.0}
 
-FLUTTER_VERSION="3.24.0"
-FLUTTER_DIR="$HOME/flutter"
-
-if [ ! -d "$FLUTTER_DIR" ]; then
-  curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz -o flutter.tar.xz
-  tar xf flutter.tar.xz -C $HOME
-fi
+git clone https://github.com/flutter/flutter.git \
+  --depth 1 \
+  --branch $FLUTTER_VERSION \
+  $HOME/flutter
 
 export PATH="$HOME/flutter/bin:$PATH"
 
 flutter --version
-
-echo "Getting packages..."
 flutter pub get
-
-echo "Building web..."
 flutter build web --release
